765418a686bd53fdadec795bd0f14954a29ad3b8
Jack Cooper <pog777six@gmail.com>
2019-03-19 14:34:46 +1100

    Change sprites, fix controls, add win/lose and ui

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,24 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
-    public float speed = 10;    //horizontal speed
+    public float speed = 0;    //horizontal speed
     public float thrustSpeed = 15;  //vertical speed
     public int carryCapacity = 3;
     public GameObject sceneController;  //current scene controller
+    public float uprightForce = 5f;
+    public float accelerateRotateForce = 1f;
+
+    public Text passengersTxt;
 
     [SerializeField] private int carrying;
     [SerializeField] private int point;
 
     private Rigidbody2D helicopter; //player
+    private Vector3 newScale;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
        helicopter = GetComponent<Rigidbody2D>();    //indentify helicopter rigidBody (player)
+        newScale = helicopter.transform.localScale;
         carrying = 0;
         point = 0;
+        if (passengersTxt) { passengersTxt.text = "Passengers: " + carrying.ToString(); }
     }
 	
 	// Update is called once per frame
@@ -26,22 +35,48 @@ public class PlayerController : MonoBehaviour {
 
         if (Input.GetButton("Horizontal") && Input.GetAxisRaw("Horizontal") > 0)
         {
+            if (newScale.x != -1)
+            {
+                newScale.x = -1;   //change direction of heli
+            }
             helicopter.AddForce(transform.right * speed);   //if d or right is pressed add +x force to helicopter
+            helicopter.AddTorque(-accelerateRotateForce);
         }
         else if (Input.GetAxisRaw("Horizontal") < 0)
         {
+            if (newScale.x != 1)
+            {
+                newScale.x = 1;   //change direction of heli
+            }
             helicopter.AddForce(transform.right * -speed);  //if a or left is pressed add -x force to helicopter
+            helicopter.AddTorque(accelerateRotateForce);
         }
+        helicopter.transform.localScale = newScale; //rotate heli
 
         if (Input.GetButton("Jump") || Input.GetButton("Vertical") && Input.GetAxisRaw("Horizontal") > 0)
         {
             helicopter.AddForce(transform.up * thrustSpeed);    //if space or w is pressed add vertical force
+
         }
         /*else if(Input.GetButton("Vertical") && Input.GetAxisRaw("Horizontal") < 0)
         {
             helicopter.AddForce(transform.up * -thrustSpeed/2);
         }
         */      //idk why this doesn't work
+
+
+        if (Input.GetButtonDown("Restart")){    //reset scene when restart button pressed
+            sceneController.GetComponent<SceneController>().ResetScene();
+        }
+
+         if(helicopter.transform.rotation.z > 0)
+        {
+            helicopter.AddTorque(-uprightForce * helicopter.transform.rotation.z);
+        }
+        else if(helicopter.transform.rotation.z < 0)
+        {
+            helicopter.AddTorque(uprightForce * -helicopter.transform.rotation.z);
+        }
 	}
 
     void OnTriggerEnter2D(Collider2D col)
@@ -53,6 +88,8 @@ public class PlayerController : MonoBehaviour {
 
             carrying++; //count how many soldiers are carried
 
+            passengersTxt.text = "Passengers: " + carrying.ToString();
+
             point = point + col.gameObject.GetComponent<Soldier>().pointValue;  //get point value from soldier
         }
 
@@ -63,6 +100,13 @@ public class PlayerController : MonoBehaviour {
 
             carrying = 0;
             point = 0;
+            passengersTxt.text = "Passengers: " + carrying.ToString();
+        }
+
+        if(col.gameObject.tag == "Tree")    //if collide with tree
+        {
+            SceneManager.LoadScene("gameOver");
+            //sceneController.GetComponent<SceneController>().ResetScene();   //come back and add game over screen
         }
     }
 }
--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,21 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneController : MonoBehaviour
 {
+
+    public Text scoreTxt;
+    //public Text passengersTxt;
+
+    private string convert;
+
     [SerializeField] int score;
 
     // Start is called before the first frame update
     void Start()
     {
         score = 0;
+        if (scoreTxt) { scoreTxt.text = "Score: " + score.ToString(); }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(score == 6)
+        {
+            SceneManager.LoadScene("win");
+        }
     }
 
     public void UpdateScore(int point)      //add point to the current score then set point to 0
@@ -23,6 +35,13 @@ public class SceneController : MonoBehaviour
         score = score + point;
         point = 0;
 
-        Debug.Log(score);
+        scoreTxt.text = "Score: " + score.ToString();
+
+        //Debug.Log(score);
+    }
+
+    public void ResetScene()
+    {
+        SceneManager.LoadScene("MASHClone");
     }
 }
